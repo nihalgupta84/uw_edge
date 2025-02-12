e049f6af7d7fce4b5fddef5161d3cac4e4cce10c
nihalgupta84 <nihalgupta84@gmail.com>
2025-02-12 15:13:43 +0530

    changed in config files

--- test.py
+++ test.py
@@ -100,7 +100,7 @@ def test():
     print(test_info)
     print(log_stats)
     os.makedirs(opt.LOG.LOG_DIR, exist_ok=True)
-    with open(os.path.join(opt.LOG.LOG_DIR, opt.TESTING.LOG_FILE), mode='a', encoding='utf-8') as f:
+    with open(os.path.join(opt.TESTING.LOG_DIR, opt.TESTING.LOG_FILE), mode='a', encoding='utf-8') as f:
         f.write(json.dumps(test_info) + '\n')
         f.write(json.dumps(log_stats) + '\n')
 
